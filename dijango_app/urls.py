d9e0dcda51f91fefaedc1f7078621bc234d58277
GTUNE\ktc <>
2025-05-08 16:29:02 +0900

    hello/ルートの追加

--- dijango_app/urls.py
+++ dijango_app/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import path
+import hello.views as hello
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('admin/', admin.site.urls),
+  path('hello/', hello.index),
 ]
