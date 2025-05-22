e9132d0dec50f3038f13b072e056fa2701276e7c
GTUNE\ktc <>
2025-05-22 16:28:29 +0900

    フォーム作成

--- hello/urls.py
+++ hello/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
   path('', views.index, name='index'),
+  path('form', views.form, name='form'),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'hello/index.html')
+  params = {
+    'title':'Hello/Index',
+    'msg':'お名前は？',
+  }
+  return render(request, 'hello/index.html', params)
+
+def form(request):
+  msg = request.POST['msg']
+  params = {
+    'title':'Hello/Form',
+    'msg':'こんにちは、' + msg + 'さん。',
+  }
+  return render(request, 'hello/index.html', params)
